2ed3238ae9bf2ea6dbaf5f8dcaf8e3e5530112e9
kernespring <kernespring@163.com>
2018-11-21 10:14:22 +0800

    Update install_tomcat.sh

--- tomcat/install_tomcat.sh
+++ tomcat/install_tomcat.sh
@@ -1,21 +1,19 @@
 TOMCAT_VERSION="apache-tomcat-8.5.34"
-TOMCAT_HOME="home/websoft/apache-tomcat-8.5.34"
+TOMCAT_HOME="/home/websoft/apache-tomcat-8.5.34"
 cd /home/websoft
 wget https://mirrors.cnnic.cn/apache/tomcat/tomcat-8/v8.5.34/bin/$TOMCAT_VERSION.tar.gz
 tar zxvf $TOMCAT_VERSION.tar.gz
-cd $TOMCAT_VERSION
 
-
+echo "export JAVA_HOME=/usr/local/java/jdk1.8.0_144" >> $TOMCAT_HOME/bin/catalina.sh
   cat > /usr/lib/systemd/system/tomcat8.service <<EOF
-export JAVA_HOME=/usr/local/java/jdk1.8.0_144
 [Unit]
 Description=Tomcat
 After=syslog.target network.target remote-fs.target nss-lookup.target
 
 [Service]
 Type=forking
-PIDFile=/$TOMCAT_HOME/tomcat.pid
-ExecStart=/$TOMCAT_HOME/bin/startup.sh
+PIDFile=$TOMCAT_HOME/tomcat.pid
+ExecStart=$TOMCAT_HOME/bin/startup.sh
 ExecReload=/bin/kill -s HUP $MAINPID
 ExecStop=/bin/kill -s QUIT $MAINPID
 PrivateTmp=true
