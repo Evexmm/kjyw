c29d44a02391a56d7705cb8a49fbb9327f3ec396
kernespring <kernespring@163.com>
2018-11-21 10:02:19 +0800

    Create install_tomcat.sh

---
+++ tomcat/install_tomcat.sh
@@ -0,0 +1,26 @@
+TOMCAT_VERSION="apache-tomcat-8.5.34"
+TOMCAT_HOME="home/websoft/apache-tomcat-8.5.34"
+wget https://mirrors.cnnic.cn/apache/tomcat/tomcat-8/v8.5.34/bin/$TOMCAT_VERSION.tar.gz
+tar zxvf $TOMCAT_VERSION.tar.gz
+cd $TOMCAT_VERSION
+
+
+  cat > /usr/lib/systemd/system/tomcat8.service <<EOF
+[Unit]
+Description=Tomcat
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=$TOMCAT_HOME/tomcat.pid
+ExecStart=$TOMCAT_HOME/bin/startup.sh
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable tomcat8.service
+systemctl start tomcat8.service
