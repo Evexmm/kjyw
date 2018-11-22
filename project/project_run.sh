cbfaf1fabfb03f2008001b0bf2f072257bebafa4
kernespring <kernespring@163.com>
2018-11-22 15:41:14 +0800

    Update project_run.sh

--- project/project_run.sh
+++ project/project_run.sh
@@ -8,7 +8,7 @@ start)
 nohup java -jar /home/websoft/java_jar/$project_name/$project_name.jar --server.port=$project_port > /opt/data/logs/run/$project_name.log 2>&1&
 ;;
 stop)
-kill -9 "`ps -ef|grep $project_port|grep -v grep|grep -v stop|awk '{print $2}'`"
+kill -9 "`"ps -ef|grep $project_port|grep -v grep|grep -v stop|awk '{print $2}'"`"
 ;;
 restart)
 kill -9 "`ps -ef|grep $project_port|grep -v grep|grep -v stop|awk '{print $2}'`"
