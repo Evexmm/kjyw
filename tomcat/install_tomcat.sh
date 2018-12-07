e97277d692917b1738c89374cf0363dcd642f82d
kernespring <kernespring@163.com>
2018-12-07 10:32:35 +0800

    Update install_tomcat.sh

--- tomcat/install_tomcat.sh
+++ tomcat/install_tomcat.sh
@@ -1,11 +1,11 @@
-TOMCAT_VERSION="apache-tomcat-8.5.34"
-TOMCAT_HOME="/home/websoft/apache-tomcat-8.5.34"
+TOMCAT_VERSION="apache-tomcat-8.5.35"
+TOMCAT_HOME="/home/websoft/apache-tomcat-8.5.35"
 cd /home/websoft
-wget https://mirrors.cnnic.cn/apache/tomcat/tomcat-8/v8.5.34/bin/$TOMCAT_VERSION.tar.gz
+wget https://mirrors.cnnic.cn/apache/tomcat/tomcat-8/v8.5.35/bin/$TOMCAT_VERSION.tar.gz
 tar zxvf $TOMCAT_VERSION.tar.gz
 
-sed -i "2iexport JAVA_HOME=/usr/local/java/jdk1.8.0_191" /home/websoft/apache-tomcat-8.5.34/bin/catalina.sh
-sed -i "3iexport JRE_HOME=/usr/local/java/jdk1.8.0_191/jre" /home/websoft/apache-tomcat-8.5.34/bin/catalina.sh
+sed -i "2iexport JAVA_HOME=/usr/local/java/jdk1.8.0_191" /home/websoft/apache-tomcat-8.5.35/bin/catalina.sh
+sed -i "3iexport JRE_HOME=/usr/local/java/jdk1.8.0_191/jre" /home/websoft/apache-tomcat-8.5.35/bin/catalina.sh
 
   cat > /usr/lib/systemd/system/tomcat8.service <<EOF
 [Unit]
@@ -14,8 +14,8 @@ After=network.target
  
 [Service]
 Type=oneshot
-ExecStart=/home/websoft/apache-tomcat-8.5.34/bin/startup.sh
-ExecStop=/home/websoft/apache-tomcat-8.5.34/bin/shutdown.sh
+ExecStart=/home/websoft/apache-tomcat-8.5.35/bin/startup.sh
+ExecStop=/home/websoft/apache-tomcat-8.5.35/bin/shutdown.sh
 ExecReload=/bin/kill -s HUP $MAINPID
 RemainAfterExit=yes
  
