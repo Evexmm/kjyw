8316e4294be7291c9de06e9e91ab66b01fb7d393
kernespring <kernespring@163.com>
2019-12-02 15:14:59 +0800

    change jdk version to 202

--- jdk/install_jdk.sh
+++ jdk/install_jdk.sh
@@ -10,27 +10,27 @@ fi
 cd /usr/local/java
 
 
-if [ ! -f "jdk-8u221-linux-x64.tar.gz" ];
+if [ ! -f "jdk-8u202-linux-x64.tar.gz" ];
 then
- wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u221-linux-x64.tar.gz
+ wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jdk-8u202-linux-x64.tar.gz
 
 fi
 
 
-if [ ! -d "jdk1.8.0_221" ];
+if [ ! -d "jdk1.8.0_202" ];
 then
- tar -xvf jdk-8u221-linux-x64.tar.gz
+ tar -xvf jdk-8u202-linux-x64.tar.gz
 fi
 
 
 #set environment
-export JAVA_HOME="/usr/local/java/jdk1.8.0_221"
-if ! grep "JAVA_HOME=/usr/local/java/jdk1.8.0_221" /etc/profile
+export JAVA_HOME="/usr/local/java/jdk1.8.0_202"
+if ! grep "JAVA_HOME=/usr/local/java/jdk1.8.0_202" /etc/profile
 then
-	echo "export JAVA_HOME=/usr/local/java/jdk1.8.0_221" | sudo tee -a  /etc/profile
+	echo "export JAVA_HOME=/usr/local/java/jdk1.8.0_202" | sudo tee -a  /etc/profile
 	echo "export PATH=\$PATH:\$JAVA_HOME/bin" | sudo tee -a /etc/profile
 	echo "export CLASSPATH=.:\$JAVA_HOME/jre/lib/rt.jar:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar" | sudo tee -a /etc/profile
 fi
 	
 source /etc/profile
-echo "jdk1.8.0_221 is installed !"
+echo "jdk1.8.0_202 is installed !"
